a74371fce6e2c293b2e606001bc72c3e8573fbbb
Joseph S. Cha <josephshincha@gmail.com>
2020-01-22 19:12:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
   # complexity.
   base_hash = {
     :railroads => {
-      :rent_in_dollars => {:one_piece_owned 
+      :rent_in_dollars => {:one_piece_owned: 
     },
   }
 end
